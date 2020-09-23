a1d39e5519d9653e8fb503aab1346c2a78ae1753
lorry0508 <1165354824@qq.com>
2020-09-23 14:07:12 +0800

    函数珂里化

---
+++ 32高频面试题书写/13.函数珂里化/index.js
@@ -0,0 +1,12 @@
+function add() {
+    const _args = [...arguments];
+    function fn() {
+        _args.push(...arguments);
+        return fn;
+    }
+    fn.toString = function() {
+        return _args.reduce((sum, cur) => sum + cur); 
+    }
+    return fn;
+}
+console.log(add(1)(2)(3)(4), ' 测试')
