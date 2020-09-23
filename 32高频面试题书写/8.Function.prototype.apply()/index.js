3370b740b851085a2de8b5e0280622caec328686
lorry0508 <1165354824@qq.com>
2020-09-23 13:43:29 +0800

    Function.prototype.apply()

---
+++ 32高频面试题书写/8.Function.prototype.apply()/index.js
@@ -0,0 +1,11 @@
+Function.prototype.apply = function(context = window, args) {
+    if (typeof this !== 'function') {
+        throw new TypeError('Type Error');
+    }
+    const fn = Symbol('fn');
+    context[fn] = this;
+    
+    const res = context[fn](...args);
+    delete context[fn];
+    return res;
+}
