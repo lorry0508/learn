161dad20770d65868200702f7f4f48b655ea935a
lorry0508 <1165354824@qq.com>
2020-09-30 13:55:53 +0800

    Promise

---
+++ 32高频面试题书写/19.深拷贝/index.js
@@ -0,0 +1,32 @@
+const cloneDeep = (target, hash = new WeakMap()) => {
+    // 对于传入参数处理
+    if (typeof target !== 'object' || target === null) {
+        return target;
+    }
+    // 哈希表中存在直接返回
+    if (hash.has(target)) return hash.get(target);
+
+    const cloneTarget = Array.isArray(target) ? [] : {};
+    hash.set(target, cloneTarget);
+
+    // 针对symbol属性
+    const symKeys = Object.getOwnPropertySymbols(target);
+    if (symKeys.length) {
+        symKeys.forEach(symKey => {
+            if (typeof target[symKey] === 'object' && target[symKey] !== null) {
+                cloneTarget[symKey] = cloneDeep(target[symKey]);
+            } else {
+                cloneTarget[symKey] = target[symKey];
+            }
+        })
+    }
+    for (const i in target) {
+        if (Object.prototype.hasOwnProperty.call(target, i)) {
+            cloneTarget[i] = 
+                typeof target[i] === 'object' && target[i] !== null
+                ? cloneDeep(target[i], hash)
+                : target[i];
+        }
+    }
+    return cloneTarget;
+};
